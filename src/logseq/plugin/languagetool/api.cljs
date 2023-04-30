da07376049656cebb29c9a7aae92297b215658f7
Avelino <avelinorun@gmail.com>
2023-04-30 11:39:07 -0300

    languagetool/api: implement /check call
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- src/logseq/plugin/languagetool/api.cljs
+++ src/logseq/plugin/languagetool/api.cljs
@@ -6,20 +6,25 @@
             [clojure.walk :as walk]
             [logseq-libs.settings :as setting]))
 
-(def languagetool-host (get (js->clj setting/settings) "languagetool-host"))
+(def languagetool-host
+  (or (get (js->clj setting/settings) "languagetool-host")
+      "https://api.languagetool.org/"))
 
 (defn check
   "Check a text"
-  [lang text]
+  ;; curl -d "text=This is an test." -d "language=auto" https://api.languagetool.org/v2/check
+  [text & {:keys [lang] :or {lang "auto"}}]
   (p/let [resp (fetch/get
                 (str languagetool-host "v2/check")
                 {:query-params {"text" text
                                 "language" lang
-                                "enabledOnly" false}})]
-    (walk/keywordize-keys (js->clj (:body resp)))))
+                                "enabledOnly" false}})
+          body (walk/keywordize-keys (js->clj (:body resp)))]
+    body))
 
 (defn languages
   "Get a list of supported languages"
   []
-  (p/let [resp (fetch/get (str languagetool-host "v2/languages"))]
-    (walk/keywordize-keys (js->clj (:body resp)))))
+  (p/let [resp (fetch/get (str languagetool-host "v2/languages"))
+          body (walk/keywordize-keys (js->clj (:body resp)))]
+    body))
